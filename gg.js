06d3538b5d9435628bcd3f92dcb86d7e982fa2dc
narmula soumya <91895795+Soumyanarmula@users.noreply.github.com>
2021-10-21 19:29:20 +0530

    soumya/project1
    music

---
+++ gg.js
@@ -0,0 +1,13 @@
+function msg()
+{
+    let num=658;
+    let sum=0;
+    let=rev;
+    while(num>0)
+    {
+        rev=num%10;
+        sum=sum*10+rev;
+        num=num/10;
+    }
+    console.log(sum);
+}
